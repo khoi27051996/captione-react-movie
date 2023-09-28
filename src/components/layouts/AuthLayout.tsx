0ee6ed50c9adc0c664978028213e0a6069d0327f
khoi <nguyenkhoi27051996@gmail.com>
2023-09-28 17:49:50 +0700

    xong part 2

--- src/components/layouts/AuthLayout.tsx
+++ src/components/layouts/AuthLayout.tsx
@@ -9,14 +9,14 @@ export const AuthLayout = () => {
     return (
         <div className="AuthLayout h-full">
             <div className="h-screen w-screen relative ">
-                <div className="absolute top-0 left-0 w-full h-full">
+                <div className="absolute top-0 left-0 w-full sm:h-full h-[100vh]">
                     <img
                         className="w-full h-full object-cover object-center"
                         src="/images/login_background.jpg"
                     />
                 </div>
                 <div className="absolute top-0 left-0 w-full h-full bg-black z-10 opacity-50 "></div>
-                <div className="absolute w-[450px] p-[30px] top-1/2 left-1/2 bg-[rgba(0,0,0,.75)] z-20 -translate-x-1/2 -translate-y-1/2 rounded-md">
+                <div className="absolute sm:w-[450px] sm:p-[30px] w-[300px] sm:p-[30px] p-[10px] top-1/2 left-1/2 bg-[rgba(0,0,0,.75)] z-20 -translate-x-1/2 -translate-y-1/2 rounded-md">
                     <Outlet />
                 </div>
             </div>
